9d7ff2634af376e800979599e3b66be7e7504c79
crvernon <chris.vernon@pnnl.gov>
2018-10-19 15:01:12 -0400

    update import initialization

--- pygis/analysis/__init__.py
+++ pygis/analysis/__init__.py
@@ -1,3 +1,5 @@
 from pygis.analysis.zonal import *
+from pygis.analysis.supply_curve import *
+from pygis.analysis.reclass import *
 
-__all__ = ['zonal']
+__all__ = ['zonal', 'supply_curve', 'reclass']
